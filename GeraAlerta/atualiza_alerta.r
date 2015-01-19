119f5da5135a6c4cff62c46fedf8d0752792b2fc
claudia-codeco <claudia.codeco@gmail.com>
2015-01-19 15:39:05 -0200

    fix small bugs corrigecasos

--- GeraAlerta/atualiza_alerta.r
+++ GeraAlerta/atualiza_alerta.r
@@ -13,18 +13,20 @@ source("fun/callmongoclima.r")
 #=============================
 # A. Organizar os dados novos:
 # ============================
-# A1. Atualizar dados de dengue:
+
+# A1. Atualizar dados de temperatura: 
+source("organizaDados/organizaTemperatura.r")
+
+# A2. Atualizar dados de tweet:
+source("organizaDados//organizatweets.r")
+
+ # A3. Atualizar dados de dengue:
 # esse e' o unico que precisa ser nominalmente indicado aqui. 
 # O dbf deve estar na pasta indicada no path dados_brutos/sinan/
 novosinan2014 <- "dados_brutos/sinan/Dengue2014_12_01_2014.dbf"
 novosinan2015 <- "dados_brutos/sinan/Dengue2015_12_01_2014.dbf"
 source("organizaDados/organizasinan.r")
 
-# A2. Atualizar dados de temperatura: 
-source("organizaDados/organizaTemperatura.r")
-
-# A3. Atualizar dados de tweet:
-source("organizaDados//organizatweets.r")
 
 # A4. Juntar todos os dados numa unica tabela
 source("organizaDados//juntaTudo.r")
@@ -33,7 +35,7 @@ source("organizaDados//juntaTudo.r")
 # B. Alerta: Para ajustar o modelo de alerta:
 # =======================================
 # Selecione os dados da semana desejada
-dadosAPS<-"dados_limpos/dadosAPS_201501.csv"
+dadosAPS<-"dados_limpos/dadosAPS_201502.csv"
 source("geraAlerta/geraAlerta.r")
 
 
--- GeraAlerta/fun/corrigecasos.r
+++ GeraAlerta/fun/corrigecasos.r
@@ -10,13 +10,13 @@ corrigecasos<-function(casos,dist="lognormal",pars=list(meanlog = 2.5016397,sdlo
   le = length(casos)
   p = rep(1,le)
   #pdig = rev(c(0.317,0.531,0.652,0.7379,0.795,0.836,0.8669,0.888,0.903,0.915,0.925,0.935,0.943,0.951,0.958,0.965,0.972,0.978,0.9822))
-  if (dist=="lognormal") pdig = rev(plnorm(seq(7,210,by=7),meanlog,sdlog))
+  if (dist=="lognormal") pdig = rev(plnorm(seq(7,210,by=7),pars$meanlog,pars$sdlog))
   p[(le-length(pdig)+1):le]<-pdig
   casos/p
 }
 
 # USO
-st$casosm<-corrigecasos(st$casos)
-tail(st)
-plot(st$casos[232:262],type="l")
-lines(st$casosm[232:262],col=2)
+#st$casosm<-corrigecasos(st$casos)
+#tail(st)
+#plot(st$casos[232:262],type="l")
+#lines(st$casosm[232:262],col=2)
