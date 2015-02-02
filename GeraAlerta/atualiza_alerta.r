34c3c0a3fd27f49f388a94cf96bb782e69833744
claudia-codeco <claudia.codeco@gmail.com>
2015-02-02 20:17:35 -0200

    atrasosinan com distr lognormal

--- GeraAlerta/atualiza_alerta.r
+++ GeraAlerta/atualiza_alerta.r
@@ -23,8 +23,8 @@ source("organizaDados//organizatweets.r")
  # A3. Atualizar dados de dengue:
 # esse e' o unico que precisa ser nominalmente indicado aqui. 
 # O dbf deve estar na pasta indicada no path dados_brutos/sinan/
-novosinan2014 <- "dados_brutos/sinan/Dengue2014_12_01_2014.dbf"
-novosinan2015 <- "dados_brutos/sinan/Dengue2015_12_01_2014.dbf"
+novosinan2014 <- "dados_brutos/sinan/Dengue2014_26_01_2015.dbf"
+novosinan2015 <- "dados_brutos/sinan/Dengue2015_26_01_2015.dbf"
 source("organizaDados/organizasinan.r")
 
 
@@ -35,18 +35,9 @@ source("organizaDados//juntaTudo.r")
 # B. Alerta: Para ajustar o modelo de alerta:
 # =======================================
 # Selecione os dados da semana desejada
-dadosAPS<-"dados_limpos/dadosAPS_201502.csv"
+dadosAPS<-"dados_limpos/dadosAPS_201504.csv"
 source("geraAlerta/geraAlerta.r")
 
 
 
 
-## OLD ################################################
-
-
-
-#knit(input="geraAlerta/geraAlerta.rmd",quiet=TRUE,envir=new.env()) # 
-#markdownToHTML("geraAlerta.md",output="html/Alerta.html", fragment.only = TRUE) 
-#alerta<-"alerta/alertaAPS_201453.csv"
-#knit(input="geraAlerta/relatorio.rmd",quiet=TRUE,envir=new.env())
-#wkhtmltopdf Alerta.html Alerta.pdf
