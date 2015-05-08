bfe2bd647553e53d9bceb710dff944834c7d1827
claudia-codeco <claudia.codeco@gmail.com>
2015-05-08 11:06:13 -0300

    adicionei arquivo faltante

--- GeraAlerta/atualiza_alerta.r
+++ GeraAlerta/atualiza_alerta.r
@@ -26,7 +26,7 @@ source("organizaDados/organizatweets.r")
 # esse e' o unico que precisa ser nominalmente indicado aqui. 
 # O dbf deve estar na pasta indicada no path dados_brutos/sinan/
 novosinan2014 <- "dados_brutos/sinan/Dengue2014_23_03_2015.dbf"
-novosinan2015 <- "dados_brutos/sinan/Dengue2015_27_04_2015.dbf"
+novosinan2015 <- "dados_brutos/sinan/Dengue04_05_2015.dbf"
 # esse source retorna os casos que nao foram geolocalizados, verificar se algum pode
 # ser resolvido. A tabela APS-bairro esta na pasta tabelas.
 source("organizaDados/organizasinan.r")
@@ -39,7 +39,7 @@ source("organizaDados//juntaTudo.r")
 # B. Alerta: Para ajustar o modelo de alerta:
 # =======================================
 # Selecione os dados da semana desejada
-dadosAPS<-"dados_limpos/dadosAPS_201516.csv"
+dadosAPS<-"dados_limpos/dadosAPS_201517.csv"
 source("geraAlerta/geraAlerta.r")
 
 
