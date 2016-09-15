1a9484178034cf9f037decc4162a87dc6d771b0d
Thais Riback <thaisriback@hotmail.com>
2016-09-15 15:33:09 -0300

    Rotina alerta

--- main/main_EPR.R
+++ main/main_EPR.R
@@ -8,12 +8,12 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 # ----- data do relatorio:
-data_relatorio = 201633
+data_relatorio = 201636
 alePR <- update.alerta(region = names(pars.PR), pars = pars.PR, crit = PR.criteria, 
                        datasource = con, sefinal=data_relatorio, writedb = FALSE)
 
 save(alePR, file="AlertaDengueAnalise/report/PR/alePR.RData")
-load("AlertaDengueAnalise/report/PR/alePR.RData") # se precisar parar e retornar depois, rode esse para nao precisar refazer o calculo do alerta
+#load("AlertaDengueAnalise/report/PR/alePR.RData") # se precisar parar e retornar depois, rode esse para nao precisar refazer o calculo do alerta
 
 # --- Gera parametros para o boletim
 configRelatorio(uf="Paraná", sigla = "PR", data=data_relatorio, 
