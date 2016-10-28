d62fe36514916c8ae8967d869b07eded92989d3f
Thais Riback <thaisriback@hotmail.com>
2016-10-28 12:58:25 -0200

    rotina alerta cascavel

--- main/main_PR_regional.R
+++ main/main_PR_regional.R
@@ -6,7 +6,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 <<<<<<< HEAD
-data_relatorio = 201641
+data_relatorio = 201642
 =======
 data_relatorio = 201635
 >>>>>>> 253682e2a38b4b4994f809f9fdb9d1b5be89f600
