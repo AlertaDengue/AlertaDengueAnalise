c487bf2f86a3e26f5feffbf7687da9b6ec42a216
Thais Riback <thaisriback@hotmail.com>
2017-02-20 14:46:24 -0300

    Rotina alerta ES

--- main/main_ES_municipios.R
+++ main/main_ES_municipios.R
@@ -6,7 +6,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 
-data_relatorio = 201705
+data_relatorio = 201706
 
 
 #***************************************************
