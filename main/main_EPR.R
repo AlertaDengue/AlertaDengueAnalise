a9b51732d7a9a08747bd197f839fbf035213d401
sara souza <sarasouzabh@gmail.com>
2017-04-09 16:58:52 -0300

    modified:   main/main_EPR.R

--- main/main_EPR.R
+++ main/main_EPR.R
@@ -8,7 +8,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 # ----- data do relatorio:
-data_relatorio = 201711
+data_relatorio = 201713
 
 alePR <- update.alerta(region = names(pars.PR), pars = pars.PR, crit = PR.criteria, 
                        datasource = con, sefinal=data_relatorio, writedb = FALSE) #, state = "Paraná"
