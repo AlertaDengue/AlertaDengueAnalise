298a5173678d76ad5cb35574e596ec24794cd731
sara souza <sarasouzabh@gmail.com>
2017-11-14 13:14:57 -0200

    modified:   main/main_ECE.R 	modified:   main/main_EPR.R

--- main/main_ECE.R
+++ main/main_ECE.R
@@ -8,7 +8,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 # ----- data do relatorio:
-data_relatorio = 201739
+data_relatorio = 201745
 
 aleCE <- update.alerta(region = names(pars.CE), state="Ceará", pars = pars.CE, crit = CE.criteria, 
                        datasource = con, sefinal=data_relatorio, writedb = FALSE, adjustdelay = TRUE) 
