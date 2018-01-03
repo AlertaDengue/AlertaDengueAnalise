293927689ec94f9a4c69fd61dcfdc6e242740e80
sara souza <sarasouzabh@gmail.com>
2018-01-03 17:22:16 -0200

    modified:   main/main_ERJ.R 	modified:   main/main_MG_regional.R 	modified:   main/main_RJ_municipios.R

--- main/main_MG_regional.R
+++ main/main_MG_regional.R
@@ -6,7 +6,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 
-data_relatorio = 201751
+data_relatorio = 201752
 
 
 # ------------------------------- 
