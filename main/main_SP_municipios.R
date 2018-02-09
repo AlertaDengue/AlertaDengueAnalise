0aba1376cc247fd20ec1179f4259e5c858861501
sara souza <sarasouzabh@gmail.com>
2018-02-09 19:04:03 -0200

    modified:   main/main_EES.R 	modified:   main/main_ES_municipios.R 	modified:   main/main_SP_municipios.R

--- main/main_SP_municipios.R
+++ main/main_SP_municipios.R
@@ -5,7 +5,7 @@ setwd("~/"); library("AlertTools")
 con <- DenguedbConnect()
 source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alerta (parametros)
 
-data_relatorio = 201804
+data_relatorio = 201805
 
 # =====
 # SJRP
@@ -15,7 +15,7 @@ aleSJRP.dengue <- update.alerta(city = 3549805, pars = pars.SP[["São José do R
                                   datasource = con, sefinal=data_relatorio, writedb = FALSE, adjustdelay = TRUE)
 # Chik tem muito pouco caso 
 aleSJRP.chick <- update.alerta(city = 3549805, pars = pars.SP[["São José do Rio Preto"]], cid10="A92.0", crit = PR.criteria, 
-#                          datasource = con, sefinal=data_relatorio, writedb = FALSE, adjustdelay = FALSE)
+                          datasource = con, sefinal=data_relatorio, writedb = FALSE, adjustdelay = FALSE)
 
 
 bolSJRP<- configRelatorioMunicipal(alert = aleSJRP.dengue, tipo = "simples", siglaUF = "SP", 
