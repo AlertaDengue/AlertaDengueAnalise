0f230fcdc50955f96ea4809deba4836d5f028fc5
sara souza <sarasouzabh@gmail.com>
2018-02-26 11:22:13 -0300

    modified:   main/main_EES.R 	modified:   main/main_ES_municipios.R 	modified:   main/main_SP_municipios.R

--- main/main_EES.R
+++ main/main_EES.R
@@ -7,7 +7,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 
 # ----- data do relatorio:
 
-data_relatorio = 201806
+data_relatorio = 201807
 
 
 # ---- Calcula alerta: 
--- main/main_SP_municipios.R
+++ main/main_SP_municipios.R
@@ -5,7 +5,7 @@ setwd("~/"); library("AlertTools")
 con <- DenguedbConnect()
 source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alerta (parametros)
 
-data_relatorio = 201805
+data_relatorio = 201807
 
 # =====
 # SJRP
