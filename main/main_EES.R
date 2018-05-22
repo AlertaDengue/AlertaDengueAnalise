2741c191b95bf16d0d0506dd7613daf9f8e08118
sara souza <sarasouzabh@gmail.com>
2018-05-22 02:18:10 -0300

    modified:   main/main_EES.R 	modified:   main/main_ES_municipios.R

--- main/main_EES.R
+++ main/main_EES.R
@@ -7,7 +7,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 
 # ----- data do relatorio:
 
-data_relatorio = 201818
+data_relatorio = 201819
 
 
 # ---- Calcula alerta: 
