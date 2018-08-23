b7b8bea294db8b54fcb56cdb8ba96ec95276dbb7
sara souza <sarasouzabh@gmail.com>
2018-08-23 08:51:48 -0300

    modified:   main/main_ECE.R 	modified:   main/main_EPR.R 	modified:   main/main_ERJ.R 	modified:   main/main_MG_regional.R 	modified:   main/main_RJ_municipios.R

--- main/main_ERJ.R
+++ main/main_ERJ.R
@@ -7,7 +7,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 
 
 # ----- data do relatorio:
-data_relatorio = 201832
+data_relatorio = 201833
 
 
 # ---- Calcula alerta Dengue: 
