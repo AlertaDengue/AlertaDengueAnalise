93e1c27900df504ac33b713d4239abaf68291e00
sara souza <sarasouzabh@gmail.com>
2018-08-26 21:16:13 -0300

    modified:   main/main_EES.R 	modified:   main/main_ES_municipios.R

--- main/main_ES_municipios.R
+++ main/main_ES_municipios.R
@@ -7,7 +7,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 con <- DenguedbConnect()
 
 
-data_relatorio = 201832
+data_relatorio = 201833
 
 aleLondrina <- update.alerta(city = 4113700, pars = pars.PR[["Londrina"]], crit = PR.criteria, 
                                   datasource = con, sefinal=data_relatorio, writedb = FALSE, adjustdelay = FALSE)
