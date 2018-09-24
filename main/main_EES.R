c27d5cc54c5db34c5512d333ea54bcd370b0235e
sara souza <sarasouzabh@gmail.com>
2018-09-23 22:24:25 -0300

    modified:   main/main_EES.R 	modified:   main/main_EPR.R 	modified:   main/main_SP_municipios.R

--- main/main_EES.R
+++ main/main_EES.R
@@ -7,7 +7,7 @@ source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alert
 
 # ----- data do relatorio:
 
-data_relatorio = 201835
+data_relatorio = 201837
 # ---- Calcula alerta: 
 
 aleES <- update.alerta(region = names(pars.ES), pars = pars.ES, state = "Espírito Santo", crit = ES.criteria, 
