407580ff4c3484288fee38c60505795da0fffa8c
sara souza <sarasouzabh@gmail.com>
2019-04-30 19:55:18 -0300

    modified:   main/main_SP_municipios.R

--- main/main_SP_municipios.R
+++ main/main_SP_municipios.R
@@ -4,7 +4,7 @@
 setwd("~/"); library("AlertTools")
 con <- DenguedbConnect()
 source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alerta (parametros)
-data_relatorio = 201911
+data_relatorio = 201916
 
 # =====
 # SJRP
