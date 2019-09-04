f713635a12fbda71dcec1b3d8a0b4f91af769b0b
sara souza <sarasouzabh@gmail.com>
2019-09-04 08:43:10 -0300

    modified:   main/main_EPR.R 	modified:   main/main_MG_regional.R

--- main/main_MG_regional.R
+++ main/main_MG_regional.R
@@ -6,7 +6,7 @@ con <- DenguedbConnect()
 source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alerta (parametros)
 
 
-data_relatorio = 201930
+data_relatorio = 201934
 
 # ------------------------------- 
 # Regional de Saude de Sete Lagoas
