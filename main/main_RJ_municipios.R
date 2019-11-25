c1ad573c9d13da3128b9f3e9932cc87fb8f06dd5
sara souza <sarasouzabh@gmail.com>
2019-11-25 16:06:27 -0200

    modified:   main/main_ECE.R 	modified:   main/main_EPR.R 	modified:   main/main_ERJ.R 	modified:   main/main_RJ_municipios.R

--- main/main_RJ_municipios.R
+++ main/main_RJ_municipios.R
@@ -5,7 +5,7 @@ con <- DenguedbConnect()
 source("AlertaDengueAnalise/config/config.R") # arquivo de configuracao do alerta (parametros)
 
 # ----- data do relatorio:
-data_relatorio = 201946
+data_relatorio = 201947
 #**********************************
 ### Cidade do Rio de Janeiro 
 #***********************************
