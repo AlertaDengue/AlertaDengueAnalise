9afa9b7403a4e444fc4a2d87ab52fea991f11b6f
esloch <es.loch@gmail.com>
2026-04-02 09:03:12 -0300

    feat(inla): install pinned INLA package and Linux binary

--- scripts/install_inla.sh
+++ scripts/install_inla.sh
@@ -1,37 +1,60 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ENV_NAME="${1:-}"
+ENV_NAME="${1:-alertadengueanalise}"
 
 if command -v mamba >/dev/null 2>&1; then
   PM="mamba"
-elif command -v conda >/dev/null 2>&1; then
-  PM="conda"
 else
-  echo "ERROR: neither 'mamba' nor 'conda' found in PATH" >&2
-  exit 127
+  PM="conda"
 fi
 
-run_in_env() {
-  if [[ -n "${ENV_NAME}" ]]; then
-    "${PM}" run -n "${ENV_NAME}" "$@"
-  else
-    "$@"
-  fi
-}
-
-run_in_env R -q --vanilla -e '
-options(timeout = 1200)
-options(repos = c(
-  CRAN = "https://cloud.r-project.org",
-  INLA = "https://inla.r-inla-download.org/R/stable"
-))
-
-install.packages(
-  "INLA",
-  dependencies = c("Depends", "Imports", "LinkingTo")
+"${PM}" run -n "${ENV_NAME}" R -q --vanilla -e '
+options(
+  timeout = 1200,
+  Ncpus = 1,
+  download.file.method = "libcurl"
 )
 
+pkg_url <- "https://inla.r-inla-download.org/R/testing/src/contrib/INLA_26.01.26-1.tar.gz"
+bin_url <- paste0(
+  "https://www.inla.r-inla-download.org/Linux-builds/",
+  "CentOS%20Linux-7%20%28Core%29%20%5Bx86_64%5D/",
+  "Version_26.01.26-1/64bit.tgz"
+)
+
+if (requireNamespace("INLA", quietly = TRUE)) {
+  remove.packages("INLA")
+}
+
+install.packages(pkg_url, repos = NULL, type = "source")
+
 suppressPackageStartupMessages(library(INLA))
-cat("OK: INLA version:", as.character(packageVersion("INLA")), "\n")
+inla_bin_linux <- system.file("bin", "linux", package = "INLA")
+
+tf <- tempfile(fileext = ".tgz")
+download.file(bin_url, tf, mode = "wb", quiet = FALSE)
+
+old_dir <- file.path(inla_bin_linux, "64bit")
+if (dir.exists(old_dir)) {
+  unlink(old_dir, recursive = TRUE, force = TRUE)
+}
+
+untar(tf, exdir = inla_bin_linux)
+
+y <- c(1, 0, 1, 0)
+x <- c(0, 1, 0, 1)
+
+res <- INLA::inla(
+  y ~ x,
+  family = "binomial",
+  data = data.frame(y = y, x = x),
+  control.compute = list(dic = FALSE, waic = FALSE, cpo = FALSE),
+  control.predictor = list(compute = FALSE),
+  num.threads = 1,
+  verbose = FALSE
+)
+
+cat("OK: INLA package version:", as.character(packageVersion("INLA")), "\n")
+cat("OK: binary installed in:", file.path(inla_bin_linux, "64bit"), "\n")
 '
